35fc605a0573823d938e59a6cb3adbd3a7d098f8
Kshitij Mehta <mehtakv@ornl.gov>
2016-05-05 14:49:46 -0400

    small exmaple without iso_c_binding

--- test.F90
+++ test.F90
@@ -1,14 +1,28 @@
-use, intrinsic :: iso_c_binding
-
-interface
-   subroutine XXX_apply(map, num), bind(C, name="XXX_apply")
-      import :: c_ptr, c_int
-      type(c_ptr), value :: map
-      integer(c_int), value :: num
-   end subroutine XXX_apply
-end interface
 
 program hello
+  implicit none
 
+
+  integer       :: i, retval
+  real(kind=8)  :: r
+  character(len=128) :: str1
+  character(len=64)  :: str2
+  character(len=32)  :: str3
+
+  !! Init variables
+  !! retval will hold the value returned from the C function
+  i=10
+  retval = 0
+  r = 10.99
+  str1 = "Hello World"//char(0)  !! Null terminate the string in fortran. Dont forget to do this.
+  str2 = ""//char(0)             !! Empty string
+  str3 = "Howdy!!"               !! Lets try a non null-terminated string
+
+  !! Call the C function
+  call c_foo(i,r,str1,str2,str3,retval)
+
+  !! Print retval returned from C
+  print *, "In Fortran code .. Back from C function, received retval: ", retval
 
 end program
+
--- test.c
+++ test.c
@@ -1,5 +1,23 @@
 #include "stdio.h"
+#include <stdlib.h>
+#include <string.h>
 
+/*
+ * The C interface to the Fortran function
+ * Notice the trailing underscore in the function name
+ * All variables are passed as pointers
+ * String lengths are passed implicitly by the compiler and not as pointers
+ */
+void c_foo_(int *i, double *r, char *str1, char *str2, char *str3, int* retval, int str1_len, int str2_len, int str3_len) {
+  puts("In C code ..");
+  printf("i: %d, r: %f, str1: %s, str2: %s, str3: %s, len_str1: %d, len_str2: %d, len_str3: %d, strlen(str1): %d, strlen(str2): %d, strlen(str3): %d, retval: %d\n",
+          *i, *r, str1, str2, str3, str1_len, str2_len, str3_len, strlen(str1), strlen(str2), strlen(str3), *retval);
+
+  //Set return value to something
+  *retval = 99;
+}
+
+/*
 void* XXX_create()
 {
   printf("XXX_create\n");
@@ -15,3 +33,4 @@ void XXX_delete(void* x)
 {
   printf("XXX_delete\n");
 }
+*/
